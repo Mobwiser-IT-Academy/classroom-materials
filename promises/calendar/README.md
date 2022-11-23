894a17264f94e1d4d6f60fb6b66ef36cd50f86cb
Tiago Aguiar <tiagoaguiar100@gmail.com>
2022-11-23 19:54:22 +0000

    Add calendar exercise

---
+++ promises/calendar/README.md
@@ -0,0 +1,6 @@
+# Code used during Mobwiser classes
+
+Exercises:
+
+Permitir mudar o país (por input ou dropdown)
+Sinalizar o dia de hoje e os anteriores aparecer em cinzento
---
+++ promises/calendar/app.js
@@ -0,0 +1,118 @@
+class Holiday {
+  constructor(date, name) {
+    this.date = date;
+    this.name = name;
+  }
+
+  getMonth() {
+    //Date format YYYY-MM-DD
+    return parseInt(this.date.split('-')[1]);
+  }
+
+  getDay() {
+    //Date format YYYY-MM-DD
+    return parseInt(this.date.split('-')[2]);
+  }
+}
+
+class Month {
+  constructor(id, name, days) {
+    this.id = id;
+    this.name = name;
+    this.days = days;
+  }
+
+  getHoliday(day, holidays) {
+    for (const holiday of holidays) {
+      if(holiday.getDay() === day && holiday.getMonth() === this.id) {
+        return holiday;
+      }
+    }
+    return null;
+  }
+}
+
+class Calendar {
+  constructor() {
+    this.months = [
+      new Month(1,'January',31),
+      new Month(2,'February',28),
+      new Month(3,'March',31),
+      new Month(4,'April',30),
+      new Month(5,'May',31),
+      new Month(6,'June',30),
+      new Month(7,'July',31),
+      new Month(8,'August',31),
+      new Month(9,'September',30),
+      new Month(10,'October',31),
+      new Month(11,'November',30),
+      new Month(12,'December',31),
+    ];
+  }
+}
+
+function fetchHolidays() {
+  let year = document.querySelector("span.year").innerHTML;
+    fetch(`https://date.nager.at/api/v2/publicholidays/${year}/PT`)
+      .then((response) => response.json())
+      .then((holidays) => renderHolidays(holidays));
+  }
+  
+function renderHolidays(holidays) {
+  let html = "";
+  holidaysArray = [];
+  for (let holiday of holidays) {
+    if(holiday.global) {
+      holidaysArray.push(new Holiday(holiday.date, holiday.name));
+      //  html += `<li class="list-holiday">
+      //    <article class="holiday-container ${Date.now() < new Date(holiday.date) ? 'color-green' : 'color-grey'}">
+      //      <h2>${holiday.name}</h2>
+      //      <p>${holiday.date}</p>
+      //    </article>
+      // </li>`;
+    }
+  }
+  document.querySelector(".holidays-list").innerHTML = html;
+  
+  renderCalendar(holidaysArray);
+}
+
+function changeYear(sum){
+  let year = document.querySelector("span.year").innerHTML;
+  year = parseInt(year) + sum;
+  document.querySelector("span.year").innerHTML = year;
+  fetchHolidays();
+}
+
+function year() {
+	let year = new Date().getFullYear();
+  document.querySelector("span.year").innerHTML = year;
+  
+  fetchHolidays();
+}
+
+function renderCalendar(holidaysArray) {
+  let calendar = new Calendar();
+  let html = "";
+  for (let month of calendar.months) {
+    html += `<li class="list-month">
+          <h2>${month.name}</h2>
+          ${renderMonth(month, holidaysArray)}
+      </li>`;
+    }
+  document.querySelector(".calendar-list").innerHTML = html;
+}
+
+function renderMonth(month, holidaysArray){
+  let html = '';
+  for(let i = 1; i <= month.days; i++) {
+    let holiday = month.getHoliday(i ,holidaysArray);
+    html += `<span class="day ${holiday ? 'holiday tooltip' : ''}">
+              ${i}
+              ${holiday ? `<span class="tooltiptext">${holiday.name}</span>` : ''}
+            </span>`
+  }
+  return `<p>${html}</p>`;
+}
+  
+fetchHolidays();
